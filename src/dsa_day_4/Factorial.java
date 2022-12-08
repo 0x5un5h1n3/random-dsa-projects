b3e5bf6ddf98b653ad75bac39e1d90b0b722d6cc
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2022-12-08 18:25:56 +0530

    Add Factorial.java

---
+++ src/dsa_day_4/Factorial.java
@@ -0,0 +1,16 @@
+package dsa_day_4;
+
+public class Factorial {
+    
+    public int fact(int c) {
+        if (c > 0) {
+            return c * fact(c - 1);
+        }
+        return 1;
+    }
+    
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+        System.out.println(f.fact(4));
+    }
+}
