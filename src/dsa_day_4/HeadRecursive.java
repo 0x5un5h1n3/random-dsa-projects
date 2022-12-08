c0760d4461f0bc249baebbb826a3605a99fffedb
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2022-12-08 18:27:03 +0530

    Head Recursive Function

---
+++ src/dsa_day_4/HeadRecursive.java
@@ -0,0 +1,17 @@
+package dsa_day_4;
+
+public class HeadRecursive {
+    
+    public void HeadRecursiveFunction(int x) {
+        
+        if (x > 0) {
+            HeadRecursiveFunction(x - 1);
+            System.out.println(x);
+        }
+    }
+    
+    public static void main(String[] args) {
+        HeadRecursive hr = new HeadRecursive();
+        hr.HeadRecursiveFunction(10);
+    }
+}
