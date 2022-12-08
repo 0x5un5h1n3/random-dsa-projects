d07e8dedc923eb4ae23926a6d98e3f281ac69173
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2022-12-08 18:30:14 +0530

    Indirect Recursive Function

---
+++ src/dsa_day_4/IndirectRecursive.java
@@ -0,0 +1,25 @@
+package dsa_day_4;
+
+public class IndirectRecursive {
+    
+    public void function1(int x) {
+        if (x > 0) {
+            System.out.println("Function 1 :" + x);
+            function2(x - 1);
+        }
+    }
+    
+    public void function2(int y) {
+        System.out.println("Function 2 " + y);
+        function3(y);
+    }
+    
+    public void function3(int z) {
+        function1(z);
+        System.out.println("Function3 " + z);
+    }
+
+    public static void main(String[] args) {
+        new IndirectRecursive().function1(10);
+    }
+}
