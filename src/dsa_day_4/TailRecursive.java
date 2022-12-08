34767151c703aa944279b4f9c89f122c3c36595d
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2022-12-08 18:27:56 +0530

    Tail Recursive Function

---
+++ src/dsa_day_4/TailRecursive.java
@@ -0,0 +1,17 @@
+package dsa_day_4;
+
+public class TailRecursive {
+    
+    public void TailRecursiveFunction(int y) {
+        
+        if (y > 0) {
+            System.out.println(y);
+            TailRecursiveFunction(y - 1);
+        }
+    }
+    
+    public static void main(String[] args) {
+        TailRecursive tr = new TailRecursive();
+        tr.TailRecursiveFunction(10);
+    }
+}
