58d4d470da2c6e07b0d7a375261b934f6b42a65f
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2022-12-12 22:14:54 +0530

    Queue

---
+++ src/dsa_day_6/Queue.java
@@ -0,0 +1,62 @@
+package dsa_day_6;
+
+public class Queue {
+
+    int maxSize = 10;
+    int front = 0;
+    int rear = 0;
+
+    int[] array = new int[maxSize];
+
+    public void enQueue(int value) {
+
+        if (maxSize == rear) {
+            System.out.println("Queue is Full");
+            return;
+        } else {
+            array[rear] = value;
+            rear++;
+        }
+    }
+
+    public void deQueue() {
+
+        if (front == rear) {
+            System.out.println("Empty Queue");
+
+        }
+        for (int i = 0; i < rear - 1; i++) {
+            array[i] = array[i + 1];
+        }
+
+        if (rear < maxSize) {
+            array[rear] = 0;
+        }
+
+    }
+
+    public void displayQueue() {
+
+        if (front == rear) {
+            System.out.println("Empty Queue");
+        }
+        for (int j = front; j < rear; j++) {
+            System.out.println("data: " + array[j]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.enQueue(100);// FIRST IN FIRST OUT
+        System.out.println("100 enQueued successfully!");
+        q.enQueue(200);
+        System.out.println("200 enQueued successfully!");
+        System.out.println("Queue displayed successfully!");
+        q.displayQueue();
+        System.out.println("Dequeue successfully!");
+        q.deQueue();
+        System.out.println("After Dequeue");
+        q.displayQueue();
+
+    }
+}
