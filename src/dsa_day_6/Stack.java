59849d63d9089b000f2b18adadc5f8578ef6af50
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2022-12-12 22:09:22 +0530

    Stack - pop()

---
+++ src/dsa_day_6/Stack.java
@@ -0,0 +1,39 @@
+package dsa_day_6;
+
+public class Stack {
+
+    int maxElementCount = 5;
+    int top = 0;
+    int[] stackData = new int[maxElementCount];
+
+    public void push(int value) {
+        if (maxElementCount > top) {
+            stackData[top++] = value;
+        } else {
+            System.out.println("Max Element Count Reached!");
+        }
+    }
+
+    public int pop() {
+        if (top > 0) {
+            return stackData[--top];
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(100);
+        s.push(200);
+        s.push(300);
+        s.push(400);
+        s.push(500);
+
+        System.out.println("pop : " + s.pop());
+        for (int i = 0; i < s.top; i++) {
+            int arg = s.stackData[i];
+            System.out.println("arg: " + arg);
+        }
+    }
+}
